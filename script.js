ddce90b97fbedf3f78031c134656f0be5f50c04c
Rafael de Andradde Prenholato <rafael.prenholato@shiftsistemas.com.br>
2023-04-07 23:32:46 -0300

    movimentos do player

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ const ctx = canvas.getContext("2d");
 
 const ballRadius = 10;
 
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 
 let dx = 2;
 let dy = -2;
+
+let rightPressed = false;
+let leftPressed = false;
 
 function drawBall() {
   ctx.beginPath();
@@ -17,19 +24,61 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(
+    paddleX,
+    canvas.height - paddleHeight - 5,
+    paddleWidth,
+    paddleHeight
+  );
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
 
-  if(x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+  if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
     dx = -dx;
   }
-  if(y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+  if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
     dy = -dy;
   }
 
   x += dx;
   y += dy;
+
+  if (rightPressed) {
+    paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+  } else if (leftPressed) {
+    paddleX = Math.max(paddleX - 7, 0);
+  }
+
+  drawPaddle();
 }
+
+function keyDownHandler(event) {
+  if (event.key === "Right" || event.key === "ArrowRight") {
+    rightPressed = true;
+  }
+  if (event.key === "Left" || event.key === "ArrowLeft") {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(event) {
+  if (event.key === "Right" || event.key === "ArrowRight") {
+    rightPressed = false;
+  }
+  if (event.key === "Left" || event.key === "ArrowLeft") {
+    leftPressed = false;
+  }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
 setInterval(draw, 10);
