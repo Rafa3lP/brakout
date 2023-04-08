68673a80473ed8700e9beaaca820e0df0ece4364
Rafael de Andradde Prenholato <rafael.prenholato@shiftsistemas.com.br>
2023-04-08 12:21:56 -0300

    eventos de mouse

--- script.js
+++ script.js
@@ -92,6 +92,11 @@ function collisionDetection() {
           dy = -dy;
           b.status = 0;
           score++;
+          if(score === brickRowCount * brickColumnCount) {
+            alert("PARABÉNS, VOCÊ GANHOU!");
+            document.location.reload();
+            clearInterval(interval); 
+          }
         }
       }
     }
@@ -116,8 +121,16 @@ function keyUpHandler(event) {
   }
 }
 
+function mouseMoveHandler(event) {
+  const relativeX = event.clientX - canvas.offsetLeft;
+  if(relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+}
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 const interval = setInterval(draw, 10);
 
