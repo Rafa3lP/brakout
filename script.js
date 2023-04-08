a6e0ec8412b1424651d38edc7a950c714f09e51a
Rafael de Andradde Prenholato <rafael.prenholato@shiftsistemas.com.br>
2023-04-07 22:55:03 -0300

    impedir que a bola ultrapasse a área

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
+const ballRadius = 10;
+
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 
@@ -9,7 +11,7 @@ let dy = -2;
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
@@ -18,6 +20,14 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+
+  if(x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+    dx = -dx;
+  }
+  if(y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+    dy = -dy;
+  }
+
   x += dx;
   y += dy;
 }
